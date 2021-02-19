7313d0563ca870512cdd024621c68a8f22250109
robertoface <reynolds.37@gmail.com>
2021-02-19 14:23:20 +0000

    quiz more sophisticated

--- growquizgrow/src/components/QuizDisplay/index.js
+++ growquizgrow/src/components/QuizDisplay/index.js
@@ -5,11 +5,12 @@ function QuizDisplay () {
 
 const [quizInfo, setQuizInfo] = useState([]);
 const [counter, setCounter] = useState(0);
-
+const [wrongCounter, setWrongCounter] = useState(1);
+const [quizLevel, setQuizLevel] = useState(`https://opentdb.com/api.php?amount=10&category=15&difficulty=easy&type=multiple`)
 
 useEffect(() => {
     async function getQuiz (){
-        let response = await fetch (`https://opentdb.com/api.php?amount=10&category=15&difficulty=easy&type=multiple`);
+        let response = await fetch (`${quizLevel}`);
         let data = await response.json();
         console.log(data.results[1]);
         setQuizInfo(data.results);
@@ -17,24 +18,39 @@ useEffect(() => {
       getQuiz();
 }, []);
 
+
+
+
+
 function handleCorrectAnswer(){
     setCounter(counter+1);
 }
 
 function handleIncorrectAnswer(){
-    alert("Wrong Answer");
+    setWrongCounter(wrongCounter+1);
+    console.log (wrongCounter);
+    // alert("Wrong Answer");
 }
+
+function refreshPage(){
+    window.location.reload();
+} 
+
 
 return (
     <div id="game-viewer">
         <h1>Points: {counter}</h1>
+        <h1>Errors: {wrongCounter-1}</h1>
+        <h1>Total: {counter+wrongCounter-1}</h1>
         {quizInfo.map((item, index) => 
             (<div key={index}> 
                 <h2>{item.question}</h2>
                 <Button arrayIncorrect={item.incorrect_answers} itemCorrect={item.correct_answer} handleCorrectClick={handleCorrectAnswer} handleIncorrectClick={handleIncorrectAnswer}></Button>
             </div>)
         )}
-    
+
+        <button type="submit" onClick={refreshPage}>Refresh Button</button>
+
     </div>
 );
 }
