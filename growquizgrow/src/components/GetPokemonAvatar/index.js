991e73f0e9b9b39bdf0b5803569af3ad4cb9a14d
alrwaishan <alrwaishanw@gmail.com>
2021-02-19 13:49:38 +0000

    Working on fetchPokemon

--- growquizgrow/src/components/GetPokemonAvatar/index.js
+++ growquizgrow/src/components/GetPokemonAvatar/index.js
@@ -1,22 +1,30 @@
 import { useEffect, useState } from 'react'
 
-function GetPokemonAvatar(id){
+function GetPokemonAvatar(){
     const [pokemon, setpokemon] = useState({});
+    const [id, setId] = useState(40);
 
     useEffect(()=>{
+        async function fetchPokemon(){
+            const result = await fetch(`${process.env.REACT_APP_POKEMONAPI_URL}${id}`);
+            const data = await result.json();
+          
+            setpokemon(data);
+        }
         fetchPokemon();
     },[id]);
 
-    async function fetchPokemon(){
-        const result = await fetch(process.env.REACT_APP_POKEMONAPI_URL+{id});
-        const data = await result.json();
-        setpokemon(data);
-    }
+    // // async function fetchPokemon(){
+    // //     const result = await fetch(process.env.REACT_APP_POKEMONAPI_URL+{id});
+    // //     const data = await result.json();
+    // //     console.log(data);
+    // //     setpokemon(data);
+    // }
 
 
     return (
         <div>
-            <img src= {pokemon.sprite.front_default}></img>
+            <img src= {pokemon.sprites?.front_default}></img>
         </div>
     )
 
@@ -25,6 +33,6 @@ function GetPokemonAvatar(id){
 
 }
 
-export default GetPokemonAvatar
+export default GetPokemonAvatar;
 // fetch pokemon inages front and back into object.
 
